31149d10cac4715d5c8c6ffe52a61fdd67324fbc
Alexandre Milev <alex92250@gmail.com>
2018-01-14 20:57:37 +0100

    interpreter + java 8

--- src/main/java/interpreter/Interpreter.java
+++ src/main/java/interpreter/Interpreter.java
@@ -12,19 +12,73 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Interpreter {
+    public Boolean isCallingBot(String message){
+        return message.toUpperCase().startsWith("PRICE ");
+    }
 
-    public String getResult(String message) {
-        String result = "NOK";
+    public String parse(String message){
+        String errors = "";
+        int limit = 1000;
+        String[] listToken = message.split(" ");
 
-        Boolean isCallingBot = message.toUpperCase().startsWith("PRICE ");
+        ArrayList<String> availableCommands = new ArrayList<>(Arrays.asList("price", "--limit"));
 
-        if (message.length() > 6) {
-            message = message.substring(6, message.length());
+        String keywords = "";
+        int index = 0;
+        while (index < listToken.length){
+            String token = listToken[index];
+            switch (token){
+                case "price":
+                    Boolean finish = false;
+                    while (!finish){
+                        index++;
+                        Boolean outOfBand = index >= listToken.length;
+                        String key = "";
+                        if(!outOfBand) {
+                            key = listToken[index];
+                            if (!availableCommands.contains(key)){
+
+                                keywords += key + " ";
+                            }else{
+                                finish = true;
+                            }
+
+                        }else{
+                            finish = true;
+                        }
+                        index --;
+                    }
+                    break;
+                case "--limit" :
+                    index ++;
+                    Boolean outOfBand = index >= listToken.length;
+                    if(!outOfBand){
+                        String key = listToken[index];
+
+                        limit = Integer.parseInt(key);
+                    }else{
+                        errors += "aucun paramètre spécifié après l'identificateur limit\n\n";
+                    }
+
+
+            }
+            index++;
+
+        }
+        if(message.isEmpty())
+            errors += "aucun mot-clé spécifié\n\n";
+
+        if(errors.isEmpty()){
+            return message;
+        }else{
+            return errors;
         }
 
-        return "";
+
     }
 
 }

d735d88787d50d571d8f89ebf96409fa5dd1ba26
ARTHUR-PC\Arthur <arthur.rozo@outlook.fr>
2018-01-14 17:41:54 +0100

    contact API

--- src/main/java/DiscordBot.java
+++ src/main/java/DiscordBot.java
@@ -1,10 +1,22 @@
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.MessageChannel;
 import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -17,7 +29,7 @@ import org.json.JSONObject;
  *
  * @author Arthur
  */
-public class DiscordBot extends ListenerAdapter{
+public class DiscordBot extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
@@ -27,15 +39,29 @@ public class DiscordBot extends ListenerAdapter{
         MessageChannel channel = event.getChannel();
         String msg = message.getContentDisplay();
         Boolean isCallingBot = msg.toUpperCase().startsWith("PRICE ");
-        
+
         if (msg.length() > 6) {
             msg = msg.substring(6, msg.length());
         }
         if (!event.getAuthor().isBot() && isCallingBot) {
-            JSONObject result = null;
+            JSONObject result = new JSONObject();
             try {
                 //JSON here
-                //result = priceApi.getPrice(msg);
+                String ur = "http://localhost:8080/priceapi/price/" + msg;
+                System.out.println(ur);
+                OkHttpClient okhttpClient = new OkHttpClient();
+                Request getRequest =new Request.Builder().url(ur).build();
+                okhttpClient.newCall(getRequest).enqueue(new Callback() {
+                    public void onFailure(Call call, IOException ioe) {
+                        System.out.println(ioe.getMessage());
+                    }
+
+                    public void onResponse(Call call, Response rspns) throws IOException {
+                        String res =rspns.body().string();
+                        System.out.println(res);
+                    }
+                });
+                
             } catch (Exception e) {
                 e.printStackTrace();
             }
